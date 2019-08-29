2bde99366c679bef2de2bce9df1c70203cafe5fb
Roberto Melara <roberto.melara627@gmail.com>
2019-08-29 01:04:54 -0600

    Minor server.js changes.

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const express = require('express');
+var mqtt = require('mqtt');
 const app = express();
 
 const data = require("./endpoints.json");
 
+var client = mqtt.connect("mqtt://192.168.1.101", {clientId:"TESTOEE"});
+
+client.on("connect", function(){	
+	console.log("Connected to MQTT Broker.");
+});
+
+client.subscribe('Test', function (err) {
+    if (!err) {
+		console.log('Succesfully connected to topic.')
+    }
+});
+
+var mqtt_data;
+
+client.on('message', function (topic, message) {
+	// message is Buffer
+	mqtt_data = message.toString();
+	//client.end()
+});
+
 for (let index = 0; index < data.endpoints.length; index++) {
 	const element = data.endpoints[index];
 	app.get(element.url, (req, res) => {
-		res.send(element.response);
-  });
-  
+		res.send(mqtt_data);
+  	});
 }
 
 // Listen to the App Engine-specified port, or 8080 otherwise
